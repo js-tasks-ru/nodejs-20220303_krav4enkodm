da4e91c006aae72d1921e57e42241e0866e3fd6b
Dmitrii Kravchenko <krav4enkodm@gmail.com>
2022-03-20 13:15:49 +0300

    Implemented limited size of stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,21 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  _limit;
+  _body = [];
+
   constructor(options) {
     super(options);
+    this._limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this._body.push(chunk);
+    if (Buffer.concat(this._body).length > this._limit) {
+      callback(new LimitExceededError(), chunk);
+      return;
+    }
+    callback(null, chunk);
   }
 }
 
